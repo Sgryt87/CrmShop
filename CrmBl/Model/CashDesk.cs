a4db049983cfd3507e293f7f7cb7d31cfa7af690
Sergii Grytsaienko <sergii.grytsaienko@accenture.com>
2019-05-07 21:44:55 -0500

    added shop model & tests

--- CrmBl/Model/CashDesk.cs
+++ CrmBl/Model/CashDesk.cs
@@ -15,6 +15,7 @@ namespace CrmBl.Model
         public int MaxQueueLength { get; set; }
         public int ExitCustomer { get; set; }
         public bool IsModel { get; set; }
+        public int Count => Queue.Count;
         public CashDesk(int number, Seller seller)
         {
             Number = number;
@@ -38,6 +39,12 @@ namespace CrmBl.Model
         public decimal Dequeue()
         {
             decimal sum = 0;
+
+            if(Queue.Count == 0)
+            {
+                return 0;
+            }
+
             var cart = Queue.Dequeue();
 
             if (cart != null)
